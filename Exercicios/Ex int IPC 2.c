35964dfd2f685d2e702b1c83e1079e60273cdd49
Eduardo Yuji Yoshida Yamada <a2320606@vmpdc.utfpr>
2023-09-27 08:00:28 -0300

    update

---
+++ Exercicios/Ex int IPC 2.c
@@ -0,0 +1,24 @@
+/*Projete um programa de cópia de arquivos chamado FileCopy 
+usando pipes comuns. Esse programa receberá dois parâmetros: 
+o primeiro é o nome do arquivo a ser 
+copiado e o segundo é o nome do arquivo copiado. Em seguida, 
+o programa criará um pipe comum e gravará nele todo conteúdo 
+do arquivo de uma só vez.
+ O processo filho lerá o conteúdo do pipe e o gravará no 
+ arquivo de destino. Por exemplo, se chamarmos o programa 
+ como descrito a seguir: 
+
+$ FileCopy entrada.txt copia.txt
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+
+
+
+    return 0;
+}
+
